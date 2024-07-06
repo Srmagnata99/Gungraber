be9311be46beef2afcf2a6778eacfca163e3d8b8
Srmagnata99 <167045603+Srmagnata99@users.noreply.github.com>
2024-07-06 00:20:48 -0300

    Gungraber

--- Gungraber.lua
+++ Gungraber.lua
@@ -7,7 +7,7 @@ local humanoidRootPart = character:WaitForChild("HumanoidRootPart") -- Parte cen
 local originalPosition = humanoidRootPart.CFrame
 
 -- Localizando o item GunDrop no Workspace
-local gunDrop = game:GetService("Workspace"):WaitForChild("GunDrop")
+local gunDrop = game:GetService("Workspace").Normal.Raggy.HumanoidRootPart
 
 -- Função para teleportar o personagem
 local function teleportTo(targetPosition)
